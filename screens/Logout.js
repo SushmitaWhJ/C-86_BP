08bef4d3ae46514e3e0636dee7728c62b474af3a
SushmitaWhJ <52954822+SushmitaWhJ@users.noreply.github.com>
2023-05-11 22:21:57 +0530

    Add files via upload

---
+++ screens/Logout.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "firebase";
+
+export default class Logout extends Component {
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Logout</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
---
+++ screens/Profile.js
@@ -0,0 +1,165 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Switch
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import * as Font from "expo-font";
+
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+import firebase from "firebase";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class Profile extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      isEnabled: false,
+      light_theme: true,
+      name: ""
+    };
+  }
+
+  toggleSwitch(){
+
+
+
+
+
+  }
+
+   async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchUser();
+  }
+
+  async fetchUser(){
+
+    
+  }
+
+
+ render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+              >
+              
+              </Image>
+            </View>
+
+            <View style={styles.appTitleTextContainer}>
+              
+
+            </View>
+
+          </View>
+
+          <View style={styles.screenContainer}>
+            <View style={styles.profileImageContainer}>
+              
+            </View>
+
+            <View style={styles.themeContainer}>
+              <Text style={styles.themeText}>Dark Theme</Text>
+
+              <Switch
+                
+              />
+            </View>
+            <View style={{ flex: 0.3 }} />
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  screenContainer: {
+    flex: 0.85
+  },
+  profileImageContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  profileImage: {
+    width: RFValue(140),
+    height: RFValue(140),
+    borderRadius: RFValue(70)
+  },
+  nameText: {
+    color: "white",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans",
+    marginTop: RFValue(10)
+  },
+  themeContainer: {
+    flex: 0.2,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: RFValue(20)
+  },
+  themeText: {
+    color: "white",
+    fontSize: RFValue(30),
+    fontFamily: "Bubblegum-Sans",
+    marginRight: RFValue(15)
+  }
+});
